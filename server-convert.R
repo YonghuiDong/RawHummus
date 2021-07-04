44752ae5c3abb213a3f09a9c335efcdd3c1d6557
Dong <yonghui.dong@gmail.com>
2021-07-04 19:42:33 +0300

    conversion function works

--- server-convert.R
+++ server-convert.R
@@ -18,18 +18,27 @@ observe({
   # 2. choose raw files
   shinyFiles::shinyFileChoose(input, "files", roots = volumes())
   if(!is.null(input$files)){
-    rawPath <- parseFilePaths(volumes(), input$files) # dataframe type
-    output$filesPath <- renderUI({HTML(paste(rawPath$datapath, sep = "", collapse = '<br/>'))})
+    rawPath <<- parseFilePaths(volumes(), input$files) # dataframe type
+    output$filesPath <- renderDataTable(rawPath)
   }
 
   # 3. choose output folder
   output$outDir <- renderText(outPath())
 
-  # 4. execute conversion
-  observeEvent(input$convert, {
-    for (i in 1:nrow(rawPath)) {
-      msconvert_command <- - paste0('"', msconvertpath(), '"', " msconvert ", '"', rawPath()[i, ], '"', " -o ", '"', outPath(), '"')
-      system(msconvert_command)
-      }
-    })
-  })
+})
+
+# 4. execute conversion
+observeEvent(input$convert, {
+  withProgress(message = "Converting...",
+               detail = "This may take a while...",
+               value = 0.4, {
+
+                 for (i in 1:nrow(rawPath)) {
+                   msconvert_command <- convert <- paste0('"', msconvertPath(), '"', " msconvert --zlib --filter ", '"', "peakPicking true [1,2]", '"', " ",'"', rawPath$datapath[i], '"', " -o ", '"', outPath(), '"')
+                   system(msconvert_command)
+                 }
+
+
+               })
+
+})
